fee939ddedea5968be846b8628b1c22fa2e4792b
geoff-schultz <gschultz98@gmail.com>
2019-09-01 16:24:16 -0500

    updating without python files

--- python_stack/my_environments/py3Env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\codingdojorepo\python_stack\my_environments\py3env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
